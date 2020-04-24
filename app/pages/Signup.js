3f38c8714b9e94e4720b1735e4199caaf1d21fcc
Peter Lu <6peterlu@gmail.com>
2020-04-23 18:47:35 -0700

    basic signup page

--- app/pages/Signup.js
+++ app/pages/Signup.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import { Appbar } from 'react-native-paper';
+import {
+  Button,
+  Subheading,
+  Title,
+  TextInput,
+} from 'react-native-paper';
+import { Platform, KeyboardAvoidingView, View } from 'react-native';
+import { signupNewUser } from '../api';
 
 const Signup = () => {
+  const [email, setEmail] = React.useState('');
+  const [pass, setPass] = React.useState('');
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [buttonLoading, setButtonLoading] = React.useState(false);
   return (
-    <Appbar.Header>
-      <Appbar.Content title="Title" subtitle="Subtitle" />
-      <Appbar.Action icon="dots-vertical" />
-    </Appbar.Header>
+    <KeyboardAvoidingView
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+      }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <Title>Murmo</Title>
+      <Subheading>where physicians collaborate</Subheading>
+      <View style={{ marginVertical: 50 }}>
+        <TextInput
+          label="Email"
+          onChangeText={setEmail}
+          style={{ width: 300 }}
+          autoCapitalize="none"
+        />
+        <TextInput
+          label="Password"
+          onChangeText={setPass}
+          style={{ width: 300 }}
+          secureTextEntry={true}
+        />
+        <TextInput
+          label="First name"
+          onChangeText={setFirstName}
+          style={{ width: 300 }}
+        />
+        <TextInput
+          label="Last name"
+          onChangeText={setLastName}
+          style={{ width: 300 }}
+        />
+      </View>
+      <Button
+        mode="contained"
+        onPress={async () => {
+          setButtonLoading(true);
+          await signupNewUser(email, pass, firstName, lastName);
+          setButtonLoading(false);
+        }}
+        disabled={!(email && pass && firstName && lastName)}
+        loading={buttonLoading}
+      >
+        Sign up
+      </Button>
+    </KeyboardAvoidingView>
   );
 };
 
